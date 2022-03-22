4a19e91753e2086baaa75dd9cb530b75b07892c9
Gonzaloav <Gonzaloav@protonmail.com>
2022-03-22 16:27:40 +0100

    suma

--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [ firstNumber, setFirstNumber ] = useState(null);
-  const [ secondNumber, setSecondNumber] = useState (null);
-  const [ result, setResult] = useState (0);
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [secondNumber, setSecondNumber] = useStat(null);
+  const [result, setResult] = useState(0);
 
-  
-  function changeFirstNumberHandler (event) {
+  function changeFirstNumberHandler(event) {
     setFirstNumber(event.target.value);
   }
 
-  function changeSecondNumberHandler (event) {
-    setSecondNumber (event.target.value);
+  function changeSecondNumberHandler(event) {
+    setSecondNumber(event.target.value);
   }
 
-  useEffect(
-    ()=>{
-      console.log("firstNumber state:", firstNumber);
-    }
-  )
-
-  useEffect(
-    ()=>{
-      console.log("secondNumber state:", secondNumber);
-    }
-  )
-
-  function addSumaHandler (result,setResult) {
-      return parseFloat (setResult);}
-   
-  
- /** function changeCheckboxHandler () {
-    setResult( result );
+  function addSumaHandler(event) {
+    setResult(firstNumber + secondNumber);
   }
-*/ 
+
+ 
+ 
+ 
+ 
+ useEffect(() => {
+    console.log("firstNumber state:", firstNumber);
+  });
+
+  useEffect(() => {
+    console.log("secondNumber state:", secondNumber);
+  });
+
+ 
   return (
     <>
       <h1>Calculadora</h1>
-         <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <input
+        type="text"
+        value={firstNumber}
+        onChange={changeFirstNumberHandler}
+      />
       <p>
-         <input type="text" value={secondNumber} onChange={changeSecondNumberHandler}/>
+        <input
+          type="text"
+          value={secondNumber}
+          onChange={changeSecondNumberHandler}
+        />
       </p>
       <p>
-        <button onClick={addSumaHandler} checked={result}> + </button>
-        <button onClick={result}> - </button>
-        <button onClick={result}> * </button>
-        <button onClick={result}> % </button>
-        <button onClick={result}> C </button>
-        <button onClick={result}> M+ </button>
-        <button onClick={result}> MR </button>
+        <button onClick={addSumaHandler} value="+">
+          {" "}
+          +{" "}
+        </button>
+        <button onClick={addRestaHandler} value="-">
+          {" "}
+          -{" "}
+        </button>
+        <button onClick={addMultiplicacionHandler} value="*">
+          {" "}
+          *{" "}
+        </button>
+        <button onClick={addDivisionHandler} value="/">
+          {" "}
+          /{" "}
+        </button>
+        <button onClick={}> C </button>
+        <button onClick={}> M+ </button>
+        <button onClick={}> MR </button>
       </p>
-      <p> 
-        <input type= "text"  value={result} onChange={addSumaHandler}/>
-      </p>
+      <p> Resultado : {result}</p>
     </>
   );
-  }
-  
+}
+
 /**<p>
         <input type="checkbox" checked={result} onChange={changeCheckboxHandler}/> 
         <button onClick={sumaContador}> + </button> />
