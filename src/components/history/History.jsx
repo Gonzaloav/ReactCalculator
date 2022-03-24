b5bad457f7d1814bebe4343cf0a5b462ba91f5e8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-24 13:28:37 +0100

    componente

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,4 +1,23 @@
 import { useEffect, useRef, useState } from "react";
-import "./App.css";
 
+/**Crea la estructura básica del componente. Ha de recibir un `prop` al que llamaremos `results`.
+ * @param {*} param0 
+ * Utiliza `results.map` para obtener un array de elementos `li`. Llama al array `liArray`.*/
+ 
 
+function historial({results}) {
+    const liArray = results.map(
+        Element=>
+                    <li>{Element}</li>  
+    )
+    // La salida del componente ha de ser un elemento `ol` que contenga los elementos de `liArray`.   
+return (
+    <>
+           <ol>
+                {liArray}
+           </ol>   
+    </>
+);        
+}
+// Y lo ponemos a disposición de otros componentes
+export default historial;
